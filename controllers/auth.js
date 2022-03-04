e92d2169696f7f1f1cb9a186dcc71fb1be7a3c36
Meruzh <meruzh.kiloyan@mail.ru>
2022-03-04 11:53:38 +0400

    login functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,19 @@
+import { users } from '../data/users.js';
+
 const login = (req, res) => {
-    res.status(200).json({ message: 'Login completed', data: req.body })
+    try {
+        const userData = req.body;
+        const newUser = users.filter(user => user.name === userData.name &&
+            user.password === userData.password);
+
+        if (newUser.length === 0) {
+            throw new Error('Username of password is incorrect')
+        }
+
+        res.status(200).json({ message: 'Login completed', data: newUser })
+    } catch (e) {
+        res.status(404).json({ message: 'Error!', err: e.message })
+    }
 }
 
 const logout = (req, res) => {
--- data/users.js
+++ data/users.js
@@ -2,42 +2,49 @@ const users = [
     {
         id: 0,
         name: 'Whitley',
+        password: '12345',
         age: 19,
         isAdmin: false
     },
     {
         id: 1,
         name: 'Todd',
+        password: '123456',
         age: 22,
         isAdmin: true
     },
     {
         id: 2,
         name: 'Montgomery',
+        password: '1234',
         age: 24,
         isAdmin: false
     },
     {
         id: 3,
         name: 'Combs',
+        password: '123',
         age: 25,
         isAdmin: true
     },
     {
         id: 4,
         name: 'Burke',
+        password: '321',
         age: 23,
         isAdmin: true
     },
     {
         id: 5,
         name: 'Lawson',
+        password: '4321',
         age: 16,
         isAdmin: false
     },
     {
         id: 6,
         name: 'Powell',
+        password: '54321',
         age: 24,
         isAdmin: true
     }
